1bdb9d10daf63ea77aa9edac2637eb496a92665a
Melina Köpsel <melina.koepsel@gmail.com>
2024-12-06 20:02:47 +0100

    allows lines to have any number of dimensions

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def normal_from_triangle(triangles):
     matrix
         (m, 3)(triangle, vector)
     """
-    a_b = triangles[:, 0, ] - triangles[:, 1, ]
-    a_c = triangles[:, 0, ] - triangles[:, 2, ]
+    a_b = triangles[..., 0, :] - triangles[..., 1, :]
+    a_c = triangles[..., 0, :] - triangles[..., 2, :]
     return np.cross(a_b, a_c)
 
 
@@ -50,9 +50,9 @@ def intersection_pln_line(triangle_pl_pts, triangle_pl_nml, line_vec, line_pts):
     """
 
     po_qo = line_pts[..., na, :] - triangle_pl_pts[na, :, :] # (Strahl, Ebene, Punkt)
-    n_po_qo = np.einsum("...ijk, ...jk->...ij", po_qo, triangle_pl_nml)[..., na] * -1
+    n_po_qo = np.einsum("...jk, jk->...j", po_qo, triangle_pl_nml)[..., na] * -1
 
-    n_p = np.einsum("...ik, ...jk->...ij", line_vec, triangle_pl_nml)[..., na]
+    n_p = np.einsum("...k, jk->...j", line_vec, triangle_pl_nml)[..., na]
 
     t = n_po_qo / n_p
     return t
@@ -78,26 +78,23 @@ def inside_out_test(triangles, normals, points):
     offset2 = np.roll(triangles, -2, axis=-2) # [c, b, a]
 
     line_vec = offset1 - offset2 # for each Point of the triangle the opposite side
-    line_normals = np.cross(line_vec, normals[:, na]) # normal of the opposite side
+    line_normals = np.cross(line_vec, normals[..., na, :]) # normal of the opposite side
 
 
-    line_nml_exp = line_normals[..., na, :, :, na, :]
-    tri_pts_exp = triangles[..., na, :, :, na, :]
+    tri_pts_exp = triangles[na, :, :, na, :]
     points_exp = points[..., na, na, :]
 
-    tri_points_br = np.broadcast_to(tri_pts_exp, (
-        (points_exp.shape[0],) + tri_pts_exp.shape[1:5])) #bc to num of inter
-
-    points_br = np.broadcast_to(points_exp,
-        (points_exp.shape[0:2] + (line_nml_exp.shape[2],) + points_exp.shape[3:5]))
+    shape = points_exp.shape[:-4] + tri_pts_exp.shape[-4:]
+    triangle_pts_br = np.broadcast_to(tri_pts_exp, shape) #bc to num of inter
+    points_br = np.broadcast_to(points_exp, shape)
 
 
-    merged = np.concatenate([tri_points_br, points_br], axis=-2)
-        # (n, m, 3, 2, 3) (tri, lin, vert, [P_tri, P_lin], xyz)
+    merged = np.concatenate([triangle_pts_br, points_br], axis=-2)
+        # ([n], m, 3, 2, 3) ([lin], tri, vert, [P_tri, P_lin], xyz)
 
     side_points = merged - offset1[na, :, :, na, :]
 
-    side_dotprods = np.einsum("iklmn, kln -> iklm", side_points, line_normals)
+    side_dotprods = np.einsum("...vmn, ...vn -> ...vm", side_points, line_normals)
     side_dotprods_prod = np.prod(side_dotprods, axis=-1)[..., na]
     side_inter_bool = side_dotprods_prod > 0
     side_inter_bool_merged = np.all(side_inter_bool, axis=-2)
@@ -115,14 +112,16 @@ def main():
         ])
 
     line_vec = np.array([
-        [7, 8, 9],
-        [10, 11, 12]
+        [[7, 8, 9],
+        [10, 11, 12],],
+        [[7, 8, 9],
+        [10, 11, 12],]
     ])
 
     #Dreiecke
     triangles = np.array([
         [[-10, -10, 0], [100, 0, 0], [0, 100, 0]],  # Triangle 1 (XY Plane)
-        [[-10, -10, 0], [0, 0, 0], [0, 100, 100]],  # Triangle 1 (XY Plane)
+        [[-10, -10, 0], [0, 0, 0], [0, 100, 100]],  # Triangle 2
         [[0, 0, 0], [1, 0, 0], [0, 0, 1]],  # Triangle 2 (XZ Plane)
         # [[0, 0, 0], [0, 1, 0], [0, 0, 1]],  # Triangle 3 (YZ Plane)
         # [[0, 0, 0], [1, 1, 0], [0, 1, 1]],  # Triangle 4 (Diagonal Plane)
@@ -154,7 +153,7 @@ def main():
 
     # inter_pts_mskd = np.where(inter_hits_mask, inter_points, np.nan)
     inter_sc_mskd = np.where(inter_hits_mask, inter_sc, np.nan)
-
+    print(inter_sc_mskd)
     inter_sc_min = np.nanmin(inter_sc_mskd, axis=-2, keepdims=True)
 >>>>>>> e643be0 (functional)
 
