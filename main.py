e02ce48d4784bdb18ac5b48f2df2f4c1bfa6bc07
TheVioletHaze <koepsel.melina@gmail.com>
2024-12-09 16:16:32 +0100

    started dimension independence

--- main.py
+++ main.py
@@ -49,10 +49,10 @@ def intersection_pln_line(triangle_pl_pts, triangle_pl_nml, line_vec, line_pts):
         (m, n, 1)(line, triangle, scalar)
     """
 
-    po_qo = line_pts[:, na, :] - triangle_pl_pts[na, :, :] # (Strahl, Ebene, Punkt)
-    n_po_qo = np.einsum("ijk, jk->ij", po_qo, triangle_pl_nml)[..., na] * -1
+    po_qo = line_pts[..., na, :] - triangle_pl_pts[na, :, :] # (Strahl, Ebene, Punkt)
+    n_po_qo = np.einsum("...ijk, ...jk->...ij", po_qo, triangle_pl_nml)[..., na] * -1
 
-    n_p = np.einsum("ik, jk->ij", line_vec, triangle_pl_nml)[..., na]
+    n_p = np.einsum("...ik, ...jk->...ij", line_vec, triangle_pl_nml)[..., na]
 
     t = n_po_qo / n_p
     return t
@@ -81,9 +81,9 @@ def inside_out_test(triangles, normals, points):
     line_normals = np.cross(line_vec, normals[:, na]) # normal of the opposite side
 
 
-    line_nml_exp = line_normals[na, :, :, na, :]
-    tri_pts_exp = triangles[na, :, :, na, :]
-    points_exp = points[:, :, na, na, :]
+    line_nml_exp = line_normals[..., na, :, :, na, :]
+    tri_pts_exp = triangles[..., na, :, :, na, :]
+    points_exp = points[..., na, na, :]
 
     tri_points_br = np.broadcast_to(tri_pts_exp, (
         (points_exp.shape[0],) + tri_pts_exp.shape[1:5])) #bc to num of inter
@@ -148,7 +148,7 @@ def main():
     triangle_pl_nml = normal_from_triangle(triangles)
     # Schnittpunkte
     inter_sc = intersection_pln_line(triangle_pl_pts, triangle_pl_nml, line_vec, line_pts)
-    inter_points = line_pts[:, na, :] + (line_vec[:, na, :] * inter_sc)
+    inter_points = line_pts[..., na, :] + (line_vec[..., na, :] * inter_sc)
 
     inter_hits_mask = inside_out_test(triangles, triangle_pl_nml, inter_points)
 
