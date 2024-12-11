6afb540747b668f92163d759442e42adf8a0945e
TheVioletHaze <koepsel.melina@gmail.com>
2024-12-11 17:36:19 +0100

    first render

--- casting.py
+++ casting.py
@@ -1,29 +1,41 @@
 import numpy as np
+from PIL import Image
 import intersection as inter
 
 def main():
     """testing method
     """
     #Strahlen
-    line_pts = np.array([
-        [1, 2, 3],
-        [4, 5, 6],
-        ])
+    n=1000
+    m=10
+    length = m / n
+    start_p=[10, -5, -5]
+    points = []
+    for i in range(0, n):
+        points_temp = []
+        for j in range(0,n):
+            points_temp.append([start_p[0], start_p[1]+(i*length), start_p[2]+(j*length)])
+        points.append(points_temp)
 
-    line_vec = np.array([
-        [7, 8, 9],
-        [10, 11, 12],
-    ])
+    points=np.array(points)
+    vectors = inter.vector_from_points(points, [0, 0, 0])
 
     #Dreiecke
     triangles = np.array([
-        [[-10, -10, 0], [100, 0, 0], [0, 100, 0]],  # Triangle 1 (XY Plane)
-        [[-10, -10, 0], [0, 0, 0], [0, 100, 100]],  # Triangle 1 (XY Plane)
-        [[0, 0, 0], [1, 0, 0], [0, 0, 1]],  # Triangle 2 (XZ Plane)
-        # [[0, 0, 0], [0, 1, 0], [0, 0, 1]],  # Triangle 3 (YZ Plane)
-        # [[0, 0, 0], [1, 1, 0], [0, 1, 1]],  # Triangle 4 (Diagonal Plane)
+        [[15, -5, -5], [20, -5, 5], [20, 5, -5]],
     ])
-    print(inter.intersection_ray_triangle(line_vec, line_pts, triangles))
+    intersections =inter.intersection_ray_triangle(vectors, points, triangles)
+
+    image=Image.new('RGB', (n,n))
+    change_pixels = image.load()
+
+    for i in range(0, n):
+        for j in range(0, n):
+            if not np.isnan(intersections[i, j, 0, 0]):
+                change_pixels[i, j] = (0, 0, 255)
+            else:
+                change_pixels[i, j] = (0, 0, 0)
+    image.show()
 
 if __name__ == "__main__":
     main()
