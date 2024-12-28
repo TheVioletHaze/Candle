15290def7ba9b458d48564c37837af4aab040438
TheVioletHaze <koepsel.melina@gmail.com>
2024-12-28 17:10:17 +0100

    dict for triangles

--- animation.py
+++ animation.py
@@ -9,7 +9,7 @@ import casting as cast
 def main(framepath):
     """testing method
     """
-    frame_num = 30
+    frame_num = 2
     decimals = len(str(frame_num)) +1
 
     if framepath.exists() and framepath.is_dir():
@@ -29,19 +29,36 @@ def main(framepath):
     #Dreiecke
     a = 3
     for i in range(0, frame_num):
-        triangles = np.array([
-            [[-10, -10, (30 + i*a)], [-14, 30, (40 + i*a)], [30, -14, (40 + i*a)]],
-            [[-3,-3, 7], [-3.2,-3.5, 7], [-3.5,-3.2, 7]],
-        ])
+        triangles = [
+            {
+                "xyz": [
+                    [-10, -10, 30 + (i*a)],
+                    [-14, 30, 40 + (i*a)],
+                    [30, -14, 40 + (i*a)]],
+                "color": [255, 255, 255],
+                "ambient": 0.3,
+                "diffuse": 0.4,
+                "specular": 0.3
+            },
+            {
+                "xyz": [
+                    [-3,-3, 7],
+                    [-3.2,-3.5, 7],
+                    [-3.5,-3.2, 7]],
+                "color": [0, 255, 0],
+                "ambient": 0.3,
+                "diffuse": 0.4,
+                "specular": 0.3
+            },
+        ]
 
-        triangles_color = np.array([
-            [255, 255, 255],
-            [0, 255, 0]
-        ])
 
-        image = cast.render_image(origin, points, triangles, triangles_color)
+        triangles_coord = np.array([tri["xyz"] for tri in triangles])
+        triangles_color = np.array([tri["color"] for tri in triangles])
+
+        image = cast.render_image(origin, points, triangles_coord, triangles_color)
         image.save(f"./frames/{i:0{decimals}d}.bmp")
-    
+
     os.system(
         f"ffmpeg -r 10/1 -start_number 1 -i \
         {framepath}/%{decimals}d.bmp -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4 -y")
