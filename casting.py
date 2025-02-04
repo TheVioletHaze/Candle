4c74f8d6c1352204a2441ef7a6adcc98d6594224
TheVioletHaze <koepsel.melina@gmail.com>
2025-02-04 14:30:21 +0100

    pixel grid with numpy index instead of for loop

--- casting.py
+++ casting.py
@@ -210,17 +210,11 @@ def pixel_grid(point_a, point_b, res_b, point_c, res_c):
 
     shift_b = vec_b / (res_b-1)
     shift_c = vec_c / (res_c-1)
+    vectors = np.array([shift_b, shift_c])
+    index_array = np.indices((res_b, res_c)).transpose(1, 2, 0)
+    array = point_a + np.sum((index_array[..., na] * vectors), axis=-2)
 
-    points = []
-    for i in range(0, res_b):
-        row = []
-        row_first = point_a + (shift_b * i)
-        for j in range(0, res_c):
-            row.append(row_first + (shift_c * j))
-
-        points.append(row)
-
-    return np.array(points)
+    return np.array(array)
 
 
 def render_image(pov, points, scene):
